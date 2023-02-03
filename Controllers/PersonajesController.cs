98353ba6eac5a68ef1dbdbcad72db13c9d7abdca
JuanjoGarrido98 <juanjogarrido98lopez@hotmail.com>
2023-02-03 16:12:21 +0100

    Add project files.

---
+++ Controllers/PersonajesController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcApiPersonajesSeries2023.Models;
+using MvcApiPersonajesSeries2023.Services;
+
+namespace MvcApiPersonajesSeries2023.Controllers
+{
+    public class PersonajesController : Controller
+    {
+        private ServiceSeries service;
+
+        public PersonajesController(ServiceSeries service)
+        {
+            this.service = service;
+        }
+
+        public async Task<IActionResult> PersonajesSerie(int idserie)
+        {
+            List<Personaje> personajes =
+                await this.service.GetPersonajesSerieAsync(idserie);
+            return View(personajes);
+        }
+
+        public async Task<IActionResult> CreatePersonaje()
+        {
+            List<Serie> series = await this.service.GetSeriesAsync();
+            ViewData["SERIES"] = series;
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreatePersonaje(Personaje personaje)
+        {
+            await this.service.CreatePersonajeAsync(personaje.IdPersonaje
+                , personaje.Nombre, personaje.Imagen, personaje.IdSerie);
+            //LO VAMOS A LLEVAR A LA VISTA PERSONAJES SERIE
+            return RedirectToAction("PersonajesSerie", new { idserie = personaje.IdSerie });
+        }
+
+        public async Task<IActionResult> UpdatePersonajeSerie()
+        {
+            List<Personaje> personajes = await this.service.GetPersonajesAsync();
+            List<Serie> series = await this.service.GetSeriesAsync();
+            ViewData["PERSONAJES"] = personajes;
+            ViewData["SERIES"] = series;
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdatePersonajeSerie
+            (int idpersonaje, int idserie)
+        {
+            await this.service.UpdateSeriePersonajeAsync(idpersonaje, idserie);
+            return RedirectToAction("PersonajesSerie"
+                , new { idserie = idserie });
+        }
+    }
+}
